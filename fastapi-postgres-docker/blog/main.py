fcaeff541869f23659c65bc5d693842b154d8cbc
deepak252 <deepakkudc16102000@gmail.com>
2024-09-22 22:03:56 +0530

    Fastapi, Postgres, Docker - Blogs api

--- fastapi-postgres-docker/blog/main.py
+++ fastapi-postgres-docker/blog/main.py
@@ -1,9 +1,17 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try: 
+        yield db
+    finally: 
+        db.close()
 
 @app.get('/')
 def index():    
@@ -11,9 +19,23 @@ def index():
         'message': 'Welcome to the Fastapi Blogs'
     }
 
-@app.post('/blog')
-def index(request: schemas.Blog):    
-    return {
-        "message": "Blog created successfully",
-        "data": request
-    }
+@app.post('/blogs')
+def create_blog(request: schemas.Blog, db: Session = Depends(get_db) ):    
+    new_blog = models.Blog(title = request.title, body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+    # return {
+    #     "message": "Blog created successfully",
+    #     "data": request
+    # }
+
+@app.get('/blogs')
+def get_blogs(db: Session = Depends(get_db) ):    
+    return db.query(models.Blog).all()
+
+@app.get('/blogs/{blog_id}')
+def get_blog(blog_id: int, db: Session = Depends(get_db) ):    
+    return db.query(models.Blog).filter(models.Blog.id==blog_id).first()
+    
